8af51e0db0a87376ec56fac0010c379c858a57ca
Arithmetics <brock.m.tillotson@gmail.com>
2017-12-06 15:55:26 -0800

    trying to fix sendgrid still, reset all email related files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'shielded-tor-39965.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
