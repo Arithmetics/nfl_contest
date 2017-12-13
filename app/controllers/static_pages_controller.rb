94ea43fd6baf794d1bb7bc3ce8215ad119cbc165
Arithmetics <brock.m.tillotson@gmail.com>
2017-12-13 12:45:15 -0800

    done, decided not really neccessary

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   before_action :admin_user, only: [:admin]
+  helper_method :sort_column, :sort_direction
 
 
   def home
@@ -16,13 +17,26 @@ class StaticPagesController < ApplicationController
   def admin
     @users = User.all
     @matchups = Matchup.all
-    @picks = Pick.all
+    @picks = Pick.order("#{sort_column} #{sort_direction}")
   end
 
   private ##################################################3
 
   def admin_user
     redirect_to(root_url) unless current_user.admin
+  end
+
+
+  def sortable_columns
+    ["user_id", "lock", "choice", "result" ]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "user_id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
